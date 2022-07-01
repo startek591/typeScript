50da6d61f5d02dc11f5e509587c1ab0398e0568f
Jonathan Fambro <startek591@gmail.com>
2022-06-30 22:18:19 -0400

    TypeScript Hello World program in Web Browsers

--- 01_hello_world/app.js
+++ 01_hello_world/app.js
@@ -1,2 +1,5 @@
 var message = 'Hello, World!';
-console.log(message);
+// create a new heading 1 element
+var heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
--- 01_hello_world/app.ts
+++ 01_hello_world/app.ts
@@ -1,3 +1,7 @@
 let message: string = 'Hello, World!';
 console.log(message);
+// create a new heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
 
